9bdc8a6991a4aadd2bdf8a9db666d7a01473d943
本柳賢太 <motoyanagikenta@motoyanagikenta-no-MacBook-Pro.local>
2016-01-05 22:28:24 +0900

    Added information dashboard to spec/example_app.

---
+++ spec/example_app/app/controllers/admin/information_controller.rb
@@ -0,0 +1,19 @@
+module Admin
+  class InformationController < Admin::ApplicationController
+    # To customize the behavior of this controller,
+    # simply overwrite any of the RESTful actions. For example:
+    #
+    # def index
+    #   super
+    #   @resources = Information.all.paginate(10, params[:page])
+    # end
+
+    # Define a custom finder by overriding the `find_resource` method:
+    # def find_resource(param)
+    #   Information.find_by!(slug: param)
+    # end
+
+    # See https://administrate-docs.herokuapp.com/customizing_controller_actions
+    # for more information
+  end
+end
